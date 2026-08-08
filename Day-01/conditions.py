012e9248b804af717077d33d1ddb84d84ea728f2
saumayraj621-sudo <saumayraj621@gmail.com>
2026-08-08 18:35:01 +0530

    Create conditions.py

---
+++ Day-01/conditions.py
@@ -0,0 +1,28 @@
+# Day 01 - Conditions
+
+age = 20
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+
+marks = 75
+
+if marks >= 90:
+    print("Excellent")
+elif marks >= 60:
+    print("Good")
+else:
+    print("Needs improvement")
+
+
+# Even or Odd
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
