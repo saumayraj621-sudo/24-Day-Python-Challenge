48eef2ac8b3a3a2565d03e2d9a539c705292e808
saumayraj621-sudo <saumayraj621@gmail.com>
2026-08-08 18:36:55 +0530

    Create nested_loops.py

---
+++ Day-01/nested_loops.py
@@ -0,0 +1,13 @@
+# Day 01 - Nested Loops
+
+for i in range(3):
+    for j in range(2):
+        print(i, j)
+
+
+# Star pattern
+
+for i in range(2):
+    for j in range(4):
+        print("*", end="")
+    print()
