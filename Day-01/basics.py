0ad3a11a4b6780f8fa0f7b0c8dd8ae7ea5af3a36
saumayraj621-sudo <saumayraj621@gmail.com>
2026-08-08 18:32:50 +0530

    Create basics.py

---
+++ Day-01/basics.py
@@ -0,0 +1,16 @@
+# Day 01 - Python Basics
+
+name = "Saumay"
+age = 20
+college = "RIT"
+
+print(name)
+print(age)
+print(college)
+
+# Taking input from the user
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+
+print("Name:", user_name)
+print("Age:", user_age)
