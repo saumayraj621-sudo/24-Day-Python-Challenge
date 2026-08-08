8606149d6cb6f06ef092f8a31ddac61e3adf3137
saumayraj621-sudo <saumayraj621@gmail.com>
2026-08-08 18:36:16 +0530

    Implement loop examples in loops.py
    Added examples of for and while loops in Python.

---
+++ Day-01/loops.py
@@ -0,0 +1,32 @@
+# Day 01 - Loops
+
+# For loop
+for i in range(5):
+    print(i)
+
+
+# Start and stop
+for i in range(2, 7):
+    print(i)
+
+
+# Step
+for i in range(1, 10, 2):
+    print(i)
+
+
+# While loop
+i = 1
+
+while i <= 5:
+    print(i)
+    i = i + 1
+
+
+# Even numbers
+i = 1
+
+while i <= 10:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1
